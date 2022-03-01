7681ff61891422e4809e7e7439df47749e80ad8a
hossamg-122 <79874817+hossamg-122@users.noreply.github.com>
2022-03-01 08:25:54 +0200

    initiate app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar/Navbar";
+//import Router from "./Router/Router";
+
+import Home from "./components/Home/Home";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { fetchContent } from "./store/actions";
+import Features from "./components/Features/Features";
 
 function App() {
+  const dispather = useDispatch();
+  useEffect(() => {
+    // this action will fetch the content of the website when the app starts for first time
+    dispather(fetchContent());
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      {/* I make the Content of the website here in main tag not a div or any other tag to follow semantic element design pattern to enhance SEO as search engines crawlers and spiders are 
+         are so sensitive to the tags and main tag stands for the content of the website
+         */}
+
+      <main>
+        <Home />
+        <Features />
+      </main>
+      {/* <Router /> */}
+    </>
   );
 }
 
